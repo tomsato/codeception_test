07f191f325c0c34d42f59c0fab70fca0a3e91ea9
tomsato <tmhr.st9711@gmail.com>
2017-06-19 02:45:12 +0000

    fix

--- tests/unit/SampleAppTest.php
+++ tests/unit/SampleAppTest.php
@@ -1,8 +1,8 @@
 <?php
 use Myapp\Controller\SampleApp;
-use Myapp\Model\SampleModel;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Http\Environment;
 use Slim\Container;
 use AspectMock\Test as test;
 
@@ -23,11 +23,11 @@ class SampleAppTest extends \Codeception\Test\Unit
     public function testResponse()
     {
         // Modelクラスをモック化する
-        test::double(new Myapp\Model\SampleModel(), [
+        test::double('Myapp\Model\SampleModel', [
             'getStr' => 'hoge'
         ]);
 
-        $env = \Slim\Http\Environment::mock([
+        $env = Environment::mock([
             'REQUEST_METHOD' => 'GET',
             'REQUEST_URI' => '/hoge/test'
         ]);
